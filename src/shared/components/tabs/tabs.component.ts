1d0061977017b18484fefb6755dd7e51198567ce
Nguyễn Đức Thịnh <90859515+ToiLaThin@users.noreply.github.com>
2023-06-13 23:51:47 +0700

    refactor styling using css variable for theme changeble (#10) (squash & merge)

--- src/shared/components/tabs/tabs.component.ts
+++ src/shared/components/tabs/tabs.component.ts
@@ -18,11 +18,14 @@ export class TabsComponent implements AfterContentInit {
     tabItemsDOM.forEach((tabItem, index) => {
       const li: HTMLElement = tabItem.nativeElement;
       const a: HTMLElement = li.querySelector('a') as HTMLElement;
-      this.renderer.setStyle(li, 'padding', '2rem');
-      this.renderer.setStyle(li, 'margin', '1rem 0');
-      this.renderer.setStyle(li, 'border', '2px solid var(--bg-primary)');
+      //dynamic renderer thì ko thể sử dụng mixin
+      this.renderer.setStyle(li, 'padding', 'var(--p-3)');
+      //this.renderer.setStyle(li, 'margin', 'var(--m-2) 0');
+      this.renderer.setStyle(li, 'border', '5px solid var(--bg-primary-clr)');
       this.renderer.setStyle(li, 'cursor', 'pointer');
       this.renderer.setStyle(a, 'color', 'whitesmoke');
+      this.renderer.setStyle(a, 'font-size', 'var(--fs-lg)');
+      this.renderer.setStyle(a, 'font-family', 'var(--ff-primary)');
     });
 
   }
